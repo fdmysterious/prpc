c7b01df36baac848ed883a0cdd1d0ca27a5de8ea
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2019-07-14 21:55:51 +0200

    Testing msg builder

---
+++ tests/msg.c
@@ -0,0 +1,37 @@
+/************************
+ * Message builder test *
+ ************************/
+// Florian Dupeyron
+// July 2019
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <prpc/msg.h>
+
+#include <test.h>
+
+int test_run()
+{
+    const char *msg_ref = "0:hello_world identifier \"string\" yes no 1 -1 1.000000 -1.000000";
+    char buf[2048];
+
+    prpc_build_msg( 0, buf, 2048,
+        "hello_world", 8,
+        TOKEN_IDENTIFIER, "identifier",
+        TOKEN_STRING    , "string",
+        TOKEN_BOOLEAN   ,    1,
+        TOKEN_BOOLEAN   ,    0,
+        TOKEN_INT       ,    1,
+        TOKEN_INT       ,   -1,
+        TOKEN_FLOAT     ,  1.0,
+        TOKEN_FLOAT     , -1.0
+    );
+
+    test_assert( strncmp( msg_ref, buf, strlen(msg_ref)) == 0, "Invalid generated string : %s", buf );
+
+    return 0;
+}
+
+void test_exit(){}
